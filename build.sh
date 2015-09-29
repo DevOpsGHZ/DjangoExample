f30f45fa7be3c4d5669fa0d8157fd2f8cfb79780
KeleiAzz <kgong@ncsu.edu>
2015-09-29 00:41:53 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-python mysite/manage.py runserver 0.0.0.0:8000 &
+sed -i  '/DEBUG = True/c\DEBUG = False' mysite/mysite/settings.py
+sed -i 's/.*ALLOWED_HOSTS.*/ALLOWED_HOSTS=["www.yourdomain.com"]/' mysite/mysite/settings.py
+python mysite/manage.py runserver 0.0.0.0:8001 &
